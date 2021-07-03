6fd58bdad42e8b27daeb5616e66f47a52b3e1e66
dwisnowski <dwisnows@gmail.com>
2021-07-02 22:48:44 -0500

    Update longest-common-prefix.js
    Runtime: 92 ms, faster than 33.77% of JavaScript online submissions for Longest Common Prefix.
    Memory Usage: 42.1 MB, less than 5.28% of JavaScript online submissions for Longest Common Prefix.

--- longest-common-prefix.js
+++ longest-common-prefix.js
@@ -2,28 +2,20 @@
  * @param {string[]} strs
  * @return {string}
  */
-var longestCommonPrefix = function (strs) {
-    if (!strs.length){
-        return ''
-    }
-
-    let longestPrefix = '';
-    let allShareSamePrefix = true;
+function getLongestPrefix(word1, word2){
     let pointer = 0;
-    while (allShareSamePrefix) {
-        let currentLetter = strs[0].charAt(pointer);
-        for (str of strs) {
-            if (str.length === 0 || str.charAt(pointer) === '' || str.charAt(pointer) !== currentLetter){
-                allShareSamePrefix = false;
-                break;
-            }
-        }
-        if (allShareSamePrefix) {
-            longestPrefix += str.charAt(pointer);
-        }
+    let prefix = '';
+    while (word1.charAt(pointer) && word2.charAt(pointer) && word1.charAt(pointer) === word2.charAt(pointer)){
+        prefix += word1.charAt(pointer);
         pointer++;
     }
-    return longestPrefix;
+    return prefix;
+}
+
+var longestCommonPrefix = function (strs) {
+    return strs.reduce((accum, str, index)=> {
+        return getLongestPrefix(str, accum);
+    }, strs[0]);
 };
 
 let expected, actual, strs;
